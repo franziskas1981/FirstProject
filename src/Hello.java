992c0ff30864aa29fdde87581d11f48389d3818c
franziskas1981 <franziska.schulz@gmail.com>
2020-11-14 16:28:14 +0100

    Adding a greeting and the current time

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,21 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Hello {
     public static void main(String[] args) {
         LocalDate current = LocalDate.now();
-        System.out.println("Hello World! Today is "+current);
+        LocalTime time = LocalTime.now();
+        String greeting;
+        int hour = time.getHour();
+
+        if (hour >= 12 && hour <= 18) {
+            greeting = "Good Afternoon!";
+        } else if (hour >=18) {
+            greeting = "Good Evening!";
+        }
+        else {
+            greeting = "Good Morning!";
+        }
+        System.out.println("Hello World! "+greeting+" Today is "+current+ " and it's currently " +time);
     }
 }
