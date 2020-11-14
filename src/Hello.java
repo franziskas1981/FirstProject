b9d1d5e69f134861de28d81fc8bd1698b659c07d
franziskas1981 <franziska.schulz@gmail.com>
2020-11-14 15:22:01 +0100

    Adding today's date!

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
+import java.time.LocalDate;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        LocalDate current = LocalDate.now();
+        System.out.println("Hello World! Today is "+current);
     }
 }
