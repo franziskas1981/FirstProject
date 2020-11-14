99dd2784abccd0b7f995bd134523d8ab531f7add
franziskas1981 <franziska.schulz@gmail.com>
2020-11-14 16:31:59 +0100

    Adding "documentation"

--- src/Hello.java
+++ src/Hello.java
@@ -3,19 +3,19 @@ import java.time.LocalTime;
 
 public class Hello {
     public static void main(String[] args) {
-        LocalDate current = LocalDate.now();
-        LocalTime time = LocalTime.now();
-        String greeting;
-        int hour = time.getHour();
+        LocalDate current = LocalDate.now(); //get current Date
+        LocalTime time = LocalTime.now(); // get current Time
+        String greeting; // initialise string to add greeting
+        int hour = time.getHour(); // get the hour of the current time
 
-        if (hour >= 12 && hour <= 18) {
+        if (hour >= 12 && hour <= 18) {  // if it's after noon, but before 6pm, greeting is "Good Afternoon"
             greeting = "Good Afternoon!";
         } else if (hour >=18) {
-            greeting = "Good Evening!";
+            greeting = "Good Evening!"; // after 6pm greeting is "Good Evening"
         }
         else {
-            greeting = "Good Morning!";
+            greeting = "Good Morning!"; // else greeting is "Good Morning"
         }
-        System.out.println("Hello World! "+greeting+" Today is "+current+ " and it's currently " +time);
+        System.out.println("Hello World! "+greeting+" Today is "+current+ " and it's currently " +time); // print line with greeting, current Date and current time
     }
 }
